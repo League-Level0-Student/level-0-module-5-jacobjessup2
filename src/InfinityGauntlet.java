eac998aada12e69cb93f7fdd500344e9c5de5335
Jessup.Jacob <Jessup.Jacob@SEMS-259-15.SEMS259.LAB>
2018-05-09 14:52:33 -0700

    for loop gauntlet

---
+++ src/InfinityGauntlet.java
@@ -0,0 +1,76 @@
+
+public class InfinityGauntlet {
+public static void main(String[] args) {
+	
+	
+/*	for (int i = 0; i < 101; i++) {
+	System.out.println(i);	
+	}
+	
+	
+	
+	for (int j = 100; j > -1; j--) {
+	System.out.println(j);	
+	}
+	
+	
+	
+	for (int x = 2; x < 102; x++) {
+		if(x%2==0) {
+			System.out.println(x);
+		}
+	}
+	
+	
+	for (int z = 1; z < 101; z++) {
+	if(z%2!=0) {
+		System.out.println(z);
+	}
+	}
+	
+	
+	for (int q = 1; q < 501; q++) {
+		if(q%2!=0) {
+			System.out.println(q + " is odd");
+		}
+		else {
+			System.out.println(q + " is even");
+		}
+		}
+	
+	
+	for (int o = 0; o < 778; o++) {
+		if(o%7==0) {
+			System.out.println(o);
+		}
+		}*/
+//	int b=0;
+//	for (int a = 2005; a < 2019; a++,b++) {
+//		System.out.println("in " + a + " I was " + b);
+//	}
+//	
+	
+//	for (int i = 0; i < 3; i++) {
+//	for (int j = 0; j < 3; j++) {
+//		System.out.println(i + " " + j);
+//	}	
+//	}
+//	
+	
+	for (int i = 0; i < 7; i++) {
+		
+		System.out.print("* ");
+		
+	}
+
+		
+		
+		
+		
+	
+	
+	
+	
+	
+}
+}
